d0e32ae64ad0b9e4e99734135aabb377e9bd5f2d
shubham kumar <41205124+Shub1970@users.noreply.github.com>
2019-04-02 19:51:13 +0530

    Update deadline.py

--- deadline.py
+++ deadline.py
@@ -1,11 +1,14 @@
 #job sequencing with deadlines
 
 def inser_profit(max_profit,profit_array,D,i):             #Recursive_function that put profit in max_profit_arra
-    if max_profit[D-1]==0:                                 #D is a deadline, and "D-1" happen because index of max_profitt_array start 
-        max_profit[D-1]=profit_array[i]                    #--with 0 index but deadline does not contain 0
+    if D>=0:
+        if max_profit[D-1]==0:                                 #D is a deadline, and "D-1" happen because index of max_profitt_array start 
+            max_profit[D-1]=profit_array[i]                    #--with 0 index but deadline does not contain 0
+            return
+         else:
+             inser_profit(max_profit,profit_array,D-1,i)
+      else:
         return
-    else:
-        inser_profit(max_profit,profit_array,D-1,i)
 if __name__=='__main__':
     job_profit={200:'j1',180:'j2',190:'j3',300:'j4',120:'j5',100:'j6'}  #don't put same value of profit
     deadline=[5,3,3,2,4,2]                              # input() for deadline
